fbbed8910ab166fb20d9a3c9bd1a14e08fb4677a
kim1893 <kim1893@purdue.edu>
2017-10-27 14:29:13 -0400

    Changed back to true

--- test-alpha/problem1_test.js
+++ test-alpha/problem1_test.js
@@ -33,8 +33,8 @@ const assert = require('chai').assert;
 const test = require('../problem1');
 
 describe('Testing problem1: isNumber()', function() {
-  it('Should return false for number', function() {
-    assert.equal(test.isNumber(1), false);
+  it('Should return true for number', function() {
+    assert.equal(test.isNumber(1), true);
   });
   it('Should return false for non-number', function() {
     assert.equal(test.isNumber('1'), false);
